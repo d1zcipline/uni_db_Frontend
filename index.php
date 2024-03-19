be0a48995348b7653d8bae5fee0d37749913a0a6
wld <d1zciplineo@gmail.com>
2024-03-19 11:29:44 +0300

    rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Customizable</title>
+  <link rel="stylesheet" href="styles/style.css">
+</head>
+
+<body>
+  <h1><a href="index.html">Customizable</a></h1>
+  <div class="wrapper">
+    <header>
+      <nav>
+        <ul>
+          <li><a href="pages/index.html">Главная</a></li>
+          <li><a href="pages/keyboards.html">Клавиатуры</a></li>
+          <li><a href="pages/info.html">Информация</a></li>
+        </ul>
+      </nav>
+      <div class="registration">
+        <ul class="button-group">
+          <li><a href="pages/login.html">Вход</a></li>
+          <li><a href="pages/registration.html">Зарегистрироваться</a></li>
+        </ul>
+      </div>
+    </header>
+    <main>
+      <div class="container">
+        <p class="main-h1"> Добро пожаловать в Customizable - компанию, которая предоставляет услуги по сборке кастомных
+          клавиатур и продаже готовых. Мы поможем вам подобрать комплектующие, организовать доставку и собрать кастомную
+          клавиатуру под ваши требования. Мы также предоставляем услуги по кастомизации, дополнительной шумоизоляции,
+          смазке переключателей и стабилизаторов. Если вы хотите приобрести механическую клавиатуру или аксессуары для
+          нее, мы также можем вам помочь. Наши цены зависят от формата (размера) клавиатуры и от количества ее клавиш.
+          Мы предлагаем услуги по сборке кастомных клавиатур разных форматов, начиная от 60% и заканчивая 100%.
+          Стоимость услуг не включает стоимость компонентов, таких как основа, переключатели, кейкапы и т.д. Мы готовы
+          помочь вам создать уникальную клавиатуру, которая будет соответствовать вашим потребностям и желаниям.
+          Свяжитесь с нами, чтобы заказать сборку кастомной клавиатуры уже сегодня!</p>
+        <img class="image" src="img/gmk67.jpg" alt="custom keyboard">
+      </div>
+      <section>
+        <h2><a href="keyboards.html">Клавиатуры</a></h2>
+        <p>Здесь будет информация о наших готовых клавиатурах.</p>
+      </section>
+    </main>
+    <footer>
+      <div class="container">
+        <div class="footer-info">
+          <h2>Контактная информация</h2>
+          <ul>
+            <li>Телефон: +7 (900) 000 00-00</li>
+            <li>Электронная почта: keyboard@custom.ru</li>
+            <li>Адрес: ул.Автозаводская, д.16, с.3</li>
+          </ul>
+        </div>
+        <div class="footer-hours">
+          <h2>Часы работы</h2>
+          <p>Ежедневно с 09:00 до 18:00</p>
+        </div>
+      </div>
+    </footer>
+  </div>
+</body>
+
+</html>
